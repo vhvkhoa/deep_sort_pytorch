2cfe26f88e5545528dc99eff2ee89c0737e3e1f7
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-04-21 12:39:12 +0700

    update

--- deepsort.py
+++ deepsort.py
@@ -12,29 +12,6 @@ from detector import build_detector
 from deep_sort import build_tracker
 from utils.draw import draw_boxes
 from utils.parser import get_config
-from utils.log import logger
-
-
-def write_results(filename, results, data_type):
-    if data_type == 'mot':
-        save_format = '{frame},{id},{x1},{y1},{w},{h},-1,-1,-1,-1\n'
-    elif data_type == 'kitti':
-        save_format = '{frame} {id} pedestrian 0 0 -10 {x1} {y1} {x2} {y2} -10 -10 -10 -1000 -1000 -1000 -10\n'
-    else:
-        raise ValueError(data_type)
-
-    with open(filename, 'w') as f:
-        for frame_id, tlwhs, track_ids in results:
-            if data_type == 'kitti':
-                frame_id -= 1
-            for tlwh, track_id in zip(tlwhs, track_ids):
-                if track_id < 0:
-                    continue
-                x1, y1, w, h = tlwh
-                x2, y2 = x1 + w, y1 + h
-                line = save_format.format(frame=frame_id, id=track_id, x1=x1, y1=y1, x2=x2, y2=y2, w=w, h=h)
-                f.write(line)
-    logger.info('save results to {}'.format(filename))
 
 
 class VideoTracker(object):
@@ -119,9 +96,6 @@ class VideoTracker(object):
 
             if self.args.save_path:
                 self.writer.write(ori_im)
-
-            # save results
-            write_results(self.result_filename, results, 'mot')
 
 
 def parse_args():
